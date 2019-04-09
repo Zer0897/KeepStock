aeceb59734990cc775ad1c26e9bbdf2c4bc77a93
Noah <noah@coronasoftware.net>
2019-04-08 18:04:06 -0700

    Started dropdown menu

--- src/widget/navigate.py
+++ src/widget/navigate.py
@@ -1,7 +1,8 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty
 from kivy.uix.behaviors.togglebutton import ToggleButtonBehavior
-
+from kivy.uix.dropdown import DropDown
+from functools import partial
 from src.widget.base import PrimaryButton
 
 
@@ -17,16 +18,49 @@ class NavigationButton(ToggleButtonBehavior, PrimaryButton):
                 w.state = 'normal'
 
 
-class NavigationBar(BoxLayout):
+class NavigationBehavior:
     items = ListProperty()
+    item_height = '20sp'
 
     def on_items(self, *args):
         group_id = ''.join(self.items)
         for item in self.items:
-            btn = NavigationButton(text=item, group=group_id)
-            self.buttons.add_widget(btn)
-            if btn.text.lower() == self.manager.current.lower():
-                btn.state = 'down'
+            self.add_button(
+                self.create_button(text=item, group=group_id)
+            )
+
+    def add_button(self, btn):
+        self.add_widget(btn)
+
+    def create_button(self, **kwds):
+        btn = NavigationButton(**kwds)
+        if btn.text.lower() == self.manager.current.lower():
+            btn.state = 'down'
+        return btn
+
+
+class NavigationBar(BoxLayout, NavigationBehavior):
+
+    def add_button(self, btn):
+        self.buttons.add_widget(btn)
+
+
+class DropDownMenu(DropDown, NavigationBehavior):
+
+    def create_button(self, **kwds):
+
+        def select(btn):
+            self.select(btn.text)
+
+        btn = NavigationButton(**kwds)
+        btn.size_hint_y = None
+        btn.height = self.item_height
+        btn.bind(on_select=select)
+        return btn
+
+
+class NavigationMenu(BoxLayout):
+    pass
 
 
 class HeaderBar(BoxLayout):
