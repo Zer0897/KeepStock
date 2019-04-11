e74f4cf731b433d2db677fdb45ccc5239e9e08a1
Noah <noah@coronasoftware.net>
2019-04-11 16:52:02 -0700

    Refactored navigation, implemented action queue

--- src/widget/navigate.py
+++ src/widget/navigate.py
@@ -1,8 +1,10 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty, ObjectProperty, StringProperty
 from kivy.uix.behaviors.togglebutton import ToggleButtonBehavior
 from kivy.uix.dropdown import DropDown
-from typing import NamedTuple
+from kivy.uix.screenmanager import ScreenManager
+from kivy.properties import ListProperty, ObjectProperty
+from kivy.clock import Clock, ClockEvent
+from typing import NamedTuple, Optional
 from src.widget.base import PrimaryButton
 
 
@@ -12,76 +14,92 @@ class NavigationItem(NamedTuple):
 
 
 class NavigationButton(ToggleButtonBehavior, PrimaryButton):
-    manager = ObjectProperty(None)
+    managers = dict()
     navigation: NavigationItem = ObjectProperty(None)
 
-    def on_release(self, *args):
-        if not self.manager.transition.is_active:
-            self.manager.current = self.text.lower()
-        self.update_state()
+    _current_event: Optional[ClockEvent] = None
 
-    def update_state(self):
-        for btn in self.parent.children:
-            if btn.navigation.screen_id == self.manager.current:
-                btn.state = 'down'
-            else:
-                btn.state = 'normal'
+    def on_press(self, *args):
+
+        def navigate(*args):
+            self.selected = True
+
+        if NavigationButton._current_event is not None:
+            NavigationButton._current_event.cancel()
+
+        if self.manager.transition.is_active:
+            duration = self.manager.transition.duration
+            progress = self.manager.current_screen.transition_progress
+            time_remaining = duration - (duration * progress)
+        else:
+            time_remaining = -1  # Instant
+
+        NavigationButton._current_event = Clock.schedule_once(navigate, time_remaining)
+
+    def update_group(self):
+        for btn in ToggleButtonBehavior.get_widgets(self.group):
+            btn.refresh()
+
+    def refresh(self):
+        self.selected = self.selected  # State of button reflects the current screen
+
+    @property
+    def selected(self) -> bool:
+        return self.navigation.screen_id == self.manager.current
+
+    @selected.setter
+    def selected(self, val: bool):
+        if val:
+            self.manager.current = self.navigation.screen_id
+            self.state = 'down'
+        else:
+            self.state = 'normal'
+
+    @property
+    def manager(self) -> ScreenManager:
+        return NavigationButton.managers.get(self.group)
 
 
-class NavigationBehavior:
+class NavigationContainerBehavior:
     manager = ObjectProperty(None)
     items = ListProperty()
     item_height = '50sp'
 
     def on_items(self, *args):
-        group_id = ''.join(item.screen_id for item in self.items)
+        btn_group = '|'.join(item.screen_id for item in self.items)
+        NavigationButton.managers[btn_group] = self.manager
         for item in self.items:
-            btn = self.create_button(group=group_id)
+            btn = self.create_button(group=btn_group)
             btn.navigation = item
             self.add_button(btn)
-            btn.update_state()
 
-    def add_button(self, btn):
+    def add_button(self, btn: NavigationButton):
         self.add_widget(btn)
 
     def create_button(self, **kwds):
-        btn = NavigationButton(**kwds)
-        btn.manager = self.manager
-        if btn.text.lower() == self.manager.current.lower():
-            btn.state = 'down'
-        return btn
+        return NavigationButton(**kwds)
 
 
-class NavigationBar(BoxLayout, NavigationBehavior):
+class NavigationBar(BoxLayout, NavigationContainerBehavior):
 
     def add_button(self, btn):
         self.buttons.add_widget(btn)
 
 
-class DropDownMenu(DropDown, NavigationBehavior):
-    manager = ObjectProperty(None)
-
-    def create_button(self, **kwds):
-
-        def select(btn):
-            self.select(btn.text)
-
-        btn = NavigationButton(**kwds)
-        btn.manager = self.manager
-        btn.size_hint_y = None
-        btn.height = self.item_height
-        btn.bind(on_select=select)
-        return btn
-
-
-class NavigationMenu(BoxLayout):
-    manager = ObjectProperty(None)
-    items = ListProperty(None)
-    item_height = StringProperty(None)
+class DropDownMenu(BoxLayout, NavigationContainerBehavior):
 
     def __init__(self, *args, **kwds):
+        self.dropdown = DropDown()
         super().__init__(*args, **kwds)
-        self.dropdown = DropDownMenu()
+
+    def create_button(self, **kwds):
+        btn = NavigationButton(**kwds)
+        btn.size_hint_y = None
+        btn.height = self.item_height
+        return btn
+
+    def add_button(self, btn):
+        self.dropdown.add_widget(btn)
 
 
 class HeaderBar(BoxLayout):
