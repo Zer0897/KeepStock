b46294fa7a54063b0e62fd42dde2618a3355c379
Noah <noah@coronasoftware.net>
2019-04-17 11:14:05 -0700

    Added descriptive error for scanner

--- src/widget/camera.py
+++ src/widget/camera.py
@@ -3,12 +3,22 @@ from kivy.garden.zbarcam.zbarcam import ZBarCam
 from kivy.clock import Clock
 
 
+class ScannerException(Exception):
+    pass
+
+
 class Scanner(ZBarCam):
 
     def capture(self, *args):
-        if not self.symbols:
-            if self.play:
-                Clock.schedule_once(self.capture)
-        else:
-            Notification.info('\n'.join(str(sym.data) for sym in self.symbols))
+        if self.symbols:
+            Notification.info(self.parse())
             self.play = False
+
+        elif self.play:
+            Clock.schedule_once(self.capture)
+
+    def parse(self):
+        try:
+            return self.symbols[0].data.decode('utf-8')
+        except IndexError as e:
+            raise ScannerException("No data detected") from e
