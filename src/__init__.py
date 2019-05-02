d4bcde7665688827eca3ff0280af2a2fa4eb81d3
Noah <noah@coronasoftware.net>
2019-05-02 15:11:20 -0700

    dev tool added to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,13 @@
 import sys
-from pathlib import Path
 from kivy.app import Builder
 from src.page import KV_FILES as page_kv
 from src.widget import KV_FILES as widget_kv
 from src.path import ROOT
+from contextlib import suppress
+
+with suppress(ImportError):
+    import stackprinter
+    stackprinter.set_excepthook(style='darkbg3')
 
 
 for kv in page_kv + widget_kv:
